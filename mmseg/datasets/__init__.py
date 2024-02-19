d689cbbc703183a5eb8ced403edb755b9549fe21
Tianrui <rayguan@umd.edu>
2024-02-19 13:10:47 -0500

    update new groups

--- mmseg/datasets/__init__.py
+++ mmseg/datasets/__init__.py
@@ -6,8 +6,11 @@ from .rugd_group6 import RUGDDataset_Group6
 from .rugd_group4 import RUGDDataset_Group4
 from .rellis_group4 import RELLISDataset_Group4
 from .cwt import CWT_Dataset
+from .rugd_group6_new import RUGDDataset_Group6_New
+from .rugd_group6_new2 import RUGDDataset_Group6_New2
 
 __all__ = [
     'CustomDataset', 'RUGDDataset', 'RELLISDataset', 'RELLISDataset_Group6', 
-    'RUGDDataset_Group6', 'RUGDDataset_Group4', 'RELLISDataset_Group4', 'CWT_Dataset'
+    'RUGDDataset_Group6', 'RUGDDataset_Group4', 'RELLISDataset_Group4', 'CWT_Dataset', 
+    'RUGDDataset_Group6_New', 'RUGDDataset_Group6_New2'
 ]
