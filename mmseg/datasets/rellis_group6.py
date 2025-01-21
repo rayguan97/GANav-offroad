80f3003452c74ee727d21f94cfa6dae1aa6ac669
Tianrui Guan <502112826@qq.com>
2025-01-21 13:43:34 -0500

    update palette

--- mmseg/datasets/rellis_group6.py
+++ mmseg/datasets/rellis_group6.py
@@ -12,8 +12,9 @@ class RELLISDataset_Group6(CustomDataset):
 
     CLASSES = ("background", "L1 (Smooth)", "L2 (Rough)", "L3 (Bumpy)", "non-Nav (Forbidden)", "obstacle")
 
-    PALETTE = [[ 108, 64, 20 ], [ 255, 229, 204 ],[ 0, 102, 0 ],[ 0, 255, 0 ],
-            [ 0, 153, 153 ],[ 0, 128, 255 ]]
+    PALETTE = [[ 0, 0, 0 ], [ 0,128,0 ],[ 255, 255, 0 ],[ 255, 128, 0 ],
+            [ 255, 0, 0 ],[  0, 0, 128] ]
+
 
     def __init__(self, **kwargs):
         super(RELLISDataset_Group6, self).__init__(
@@ -21,5 +22,5 @@ class RELLISDataset_Group6(CustomDataset):
             seg_map_suffix='_group6.png',
             **kwargs)
         self.CLASSES = ("background", "L1 (Smooth)", "L2 (Rough)", "L3 (Bumpy)", "non-Nav (Forbidden)", "obstacle")
-        self.PALETTE = [[ 108, 64, 20 ], [ 255, 229, 204 ],[ 0, 102, 0 ],[ 0, 255, 0 ],
-            [ 0, 153, 153 ],[ 0, 128, 255 ]]
+        self.PALETTE = [[ 0, 0, 0 ], [ 0,128,0 ],[ 255, 255, 0 ],[ 255, 128, 0 ],
+            [ 255, 0, 0 ],[  0, 0, 128] ]
